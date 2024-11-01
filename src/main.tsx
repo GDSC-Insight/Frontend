686c7c311f2f1b5aea3b4f50e5726896b5035b24
mimpie <yesl3715@naver.com>
2024-11-01 20:16:39 +0900

    Init

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <h1>Main</h1>
   </StrictMode>
 );
