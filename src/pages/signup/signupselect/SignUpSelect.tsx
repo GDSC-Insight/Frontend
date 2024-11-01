31438344aac9eba8e091e0be230714adc0cf7972
returnseo <reseo4150@gmail.com>
2024-11-02 00:43:08 +0900

    로그인 및 회원가입

---
+++ src/pages/signup/signupselect/SignUpSelect.tsx
@@ -0,0 +1,27 @@
+import './SignUpSelect.css'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function SignUpSelect() {
+
+  const navigator = useNavigate(); 
+
+  const onClickCoporation = () => {
+    navigator("/corporationsignup")
+  }
+
+  const onClickUser = () => {
+    navigator("/usersignup")
+  }
+ 
+  return (
+    <div id='signup-select-warpper'>
+        <div className='select-box' onClick={onClickCoporation}>
+        <div className='select-text coporation'>기업</div>  
+        </div>
+        <div className='select-box' onClick={onClickUser}>
+        <div className='select-text user' >개인</div>
+        </div>
+    </div>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -3,8 +3,13 @@ import Main from "./pages/Main";
 import Auth from "./pages/auth/Auth";
 import AuthSelect from "./pages/auth/authselect/AuthSelect";
 import CoporationAuth from "./pages/auth/coporationauth/CoporationAuth";
+import UserAuth from "./pages/auth/userauth/UserAuth";
+import SignUpSelect from "./pages/signup/signupselect/SignUpSelect";
+import UserSignUp from "./pages/signup/usersignup/UserSignUp";
 
 const router = createBrowserRouter([
+   
+
   {
     path: "/",
     element: <Main />,
@@ -20,7 +25,24 @@ const router = createBrowserRouter([
   {
     path: "/corporationauth",
     element: <CoporationAuth/>
-  }
+  },
+  {
+    path: "/userauth",
+    element: <UserAuth/>
+  },
+  {
+    path: "/signupselect",
+    element: <SignUpSelect/>
+  },
+  {
+    path: "/usersignup",
+    element: <UserSignUp/>
+  },
+  {
+    path: "/corporationsignup",
+    element: <UserAuth/>
+  },
+
 ]);
 
 export default router;
